3e842ca807ea35242a3b274112cc0e0a7690326f
Shawn Harrington <shawn.harrington2776@gmail.com>
2025-01-26 01:30:39 -0800

    removed logs, db working

--- app/actions/getUsers.js
+++ app/actions/getUsers.js
@@ -3,8 +3,6 @@ import { query } from "@/lib/db";
 export async function getUsers() {
   try {
     const res = await query(`SELECT * FROM users`);
-    console.log(res.rows);
-    console.log("test");
 
     return res.rows;
   } catch (error) {
