ed25e6e57991895f17bfda85a4a66e58cc479282
Shawn Harrington <shawn.harrington2776@gmail.com>
2025-01-26 01:23:47 -0800

    logging

--- app/actions/getUsers.js
+++ app/actions/getUsers.js
@@ -3,6 +3,8 @@ import { query } from "@/lib/db";
 export async function getUsers() {
   try {
     const res = await query(`SELECT * FROM users`);
+    console.log(res.rows);
+
     return res.rows;
   } catch (error) {
     console.log(error);
